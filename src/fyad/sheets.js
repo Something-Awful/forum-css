52180ae751ed7298ff6bcf0fdd2887ae53195563
astral <astral@somethingawful.com>
2021-10-26 00:16:42 -0700

    Harmonize sheets.js formatting

--- src/fyad/sheets.js
+++ src/fyad/sheets.js
@@ -1,69 +1,69 @@
 window.fyadsheets = {
-    "1": {
-        "name": "FYAD Vice",
-        "path": "fyadvice"
-    },
-    "2": {
-        "name": "Gas Chamber",
-        "path": "gaschamber"
-    },
-    "4": {
-        "name": "Nighttime FYAD",
-        "path": "night",
-        "hours": [21, 22, 23, 0, 1, 2, 3, 4, 5]
-    },
-    "ralp": {
-        "name": "Ralp's Theme",
-        "path": "ralp"
-    },
-    "5": {
-        "name": "Sheep Taking A Shit",
-        "path": "sheep"
-    },
-    "6": {
-        "name": "Day Crew FYAD",
-        "path": "sports",
-        "hours": [10, 11, 12, 13, 14, 15]
-    },
-    "7": {
-        "name": "The Cool Dude Forums",
-        "path": "cooldude"
-    },
-    "8": {
-        "name": "Techno Party (by lmaoboy1998)",
-        "path": "technoparty"
-    },
-    "9": {
-        "name": "Pumpkin Spice (by Skylark)",
-        "path": "pumpkin"
-    },
-    "10": {
-        "name": "Relatable Fall (by Piss Turbo)",
-	"path": "relatablefall"
-    },
-    "11": {
-        "name": "Persona 4 (by duncecap)",
-	"path": "anime"
-    },
-    "12": {
-        "name": "Summertime (by Piss Turbo)",
-	"path": "summertime",
-        "hours": [16, 17, 18, 19, 20]
-    },
-    "13": {
-        "name": "Metal Gear Solid (by duncecap)",
-	"path": "mgs"
-    },
-    "14": {
-        "name": "Cottagecore Posting Canopy (by Skylark)",
-	"path": "cottagecore"
-    },
-    "15": {
-        "name": "Dreamfyasd (by Skylark)",
-	"path": "dreamfyasd"
-    },
-    "16": {
-        "name": "Vaporwave (by Pog)",
-	"path": "vaporwave"
-    }
+	"1": {
+		"name": "FYAD Vice",
+		"path": "fyadvice"
+	},
+	"2": {
+		"name": "Gas Chamber",
+		"path": "gaschamber"
+	},
+	"4": {
+		"name": "Nighttime FYAD",
+		"path": "night",
+		"hours": [21, 22, 23, 0, 1, 2, 3, 4, 5]
+	},
+	"ralp": {
+		"name": "Ralp's Theme",
+		"path": "ralp"
+	},
+	"5": {
+		"name": "Sheep Taking A Shit",
+		"path": "sheep"
+	},
+	"6": {
+		"name": "Day Crew FYAD",
+		"path": "sports",
+		"hours": [10, 11, 12, 13, 14, 15]
+	},
+	"7": {
+		"name": "The Cool Dude Forums",
+		"path": "cooldude"
+	},
+	"8": {
+		"name": "Techno Party (by lmaoboy1998)",
+		"path": "technoparty"
+	},
+	"9": {
+		"name": "Pumpkin Spice (by Skylark)",
+		"path": "pumpkin"
+	},
+	"10": {
+		"name": "Relatable Fall (by Piss Turbo)",
+		"path": "relatablefall"
+	},
+	"11": {
+		"name": "Persona 4 (by duncecap)",
+		"path": "anime"
+	},
+	"12": {
+		"name": "Summertime (by Piss Turbo)",
+		"path": "summertime",
+		"hours": [16, 17, 18, 19, 20]
+	},
+	"13": {
+		"name": "Metal Gear Solid (by duncecap)",
+		"path": "mgs"
+	},
+	"14": {
+		"name": "Cottagecore Posting Canopy (by Skylark)",
+		"path": "cottagecore"
+	},
+	"15": {
+		"name": "Dreamfyasd (by Skylark)",
+		"path": "dreamfyasd"
+	},
+	"16": {
+		"name": "Vaporwave (by Pog)",
+		"path": "vaporwave"
+	}
 };
